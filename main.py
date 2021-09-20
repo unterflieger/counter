b2cefe2ddfaf2446d7eab07bb6c00f28ff2d4e9b
unterflieger010@gmail.com <unterflieger>
2021-09-20 13:08:45 +0200

    Removed cps counter

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import time
 val = int(0)
 cps = int(0)
 
-layout = [[gui.Button("Add 1")],[gui.Text(str(val), key='count')],[gui.Text('CPS: '+str(cps), key='cpscount')]]
+layout = [[gui.Button("Add 1")],[gui.Text(str(val), key='count')],]
 window = gui.Window("Hello World", layout, margins=(100, 50))
 while True:
     event, values = window.read()
     if event == "Add 1":
         val = val + int(1)
         window['count'].update(val)
-        window['cpscount'].update('CPS: ' + str(cps))
     if event == gui.WIN_CLOSED:
         break
